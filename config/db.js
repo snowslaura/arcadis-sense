f4b80af80f5ea160d10ec4dad731f768de5dd98e
Laura Neves <lauracmneves@gmail.com>
2022-07-13 11:45:20 -0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import pg from "pg";
+import dotenv from "dotenv"
+dotenv.config()
+
+const { Pool } = pg;
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+};
+
+const db = new Pool(configDatabase);
+export default db;
+
+
